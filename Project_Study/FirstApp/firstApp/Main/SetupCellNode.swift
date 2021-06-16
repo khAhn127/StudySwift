1baeebf041bb7cba090c9e14c1ddc10a89dfc425
kwanghun <kh.ahn@quintet.co.kr>
2021-06-16 14:11:43 +0900

    add - set SetupCellNode

--- Project_Study/FirstApp/firstApp/Main/SetupCellNode.swift
+++ Project_Study/FirstApp/firstApp/Main/SetupCellNode.swift
@@ -17,10 +17,10 @@ class SetupCellNode: ASCellNode {
         node.attributedText = NSAttributedString(
             string: "이름",
             attributes: [.font: UIFont.boldSystemFont(ofSize: 12),
-                         .foregroundColor : UIColor.white,
+                         .foregroundColor : UIColor.black,
                          .paragraphStyle : paragraphStyle,
             ])
-        node.borderColor = UIColor.black.cgColor
+        node.borderColor = UIColor.red.cgColor
         node.borderWidth = 0.5
         return node
     }()
@@ -28,13 +28,39 @@ class SetupCellNode: ASCellNode {
     lazy var inputNode : ASEditableTextNode = {
         let node = ASEditableTextNode()
         node.backgroundColor = UIColor.white
-        node.borderColor = UIColor.black.cgColor
+        node.borderColor = UIColor.blue.cgColor
         node.borderWidth = 0.5
         return node
     }()
     
     override init() {
         super.init()
+        self.automaticallyManagesSubnodes = true
+        self.automaticallyRelayoutOnLayoutMarginsChanges = true
+        self.automaticallyRelayoutOnSafeAreaChanges = true
+        self.style.width = .init(unit: .fraction, value: 1)
+    }
+    
+    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        return ASInsetLayoutSpec(
+            insets: .zero,
+            child: ASStackLayoutSpec(
+                direction: .horizontal,
+                spacing: 0,
+                justifyContent: .spaceBetween,
+                alignItems: .stretch,
+                children: [
+                    titleNode.styled({
+                        $0.width = .init(unit: .fraction, value: 0.5)
+                        $0.height = .init(unit: .points, value: 20)
+                    }),
+                    inputNode.styled({
+                        $0.width = .init(unit: .fraction, value: 0.5)
+                        $0.height = .init(unit: .points, value: 20)
+                    }),
+                ]
+            )
+        )
     }
     
 }
