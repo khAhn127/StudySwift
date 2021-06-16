05a7129300af87971468638dd314fbe8a281db39
kwanghun <kh.ahn@quintet.co.kr>
2021-06-16 10:54:53 +0900

    add - titleNode, inputNode

--- Project_Study/FirstApp/firstApp/Main/SetupCellNode.swift
+++ Project_Study/FirstApp/firstApp/Main/SetupCellNode.swift
@@ -6,7 +6,35 @@
 //
 
 import UIKit
+import AsyncDisplayKit
 
-//class SetupCellNode: ASCellNode {
-//
-//}
+class SetupCellNode: ASCellNode {
+
+    lazy var titleNode : ASTextNode = {
+        let node = ASTextNode()
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        node.attributedText = NSAttributedString(
+            string: "이름",
+            attributes: [.font: UIFont.boldSystemFont(ofSize: 12),
+                         .foregroundColor : UIColor.white,
+                         .paragraphStyle : paragraphStyle,
+            ])
+        node.borderColor = UIColor.black.cgColor
+        node.borderWidth = 0.5
+        return node
+    }()
+    
+    lazy var inputNode : ASEditableTextNode = {
+        let node = ASEditableTextNode()
+        node.backgroundColor = UIColor.white
+        node.borderColor = UIColor.black.cgColor
+        node.borderWidth = 0.5
+        return node
+    }()
+    
+    override init() {
+        super.init()
+    }
+    
+}
