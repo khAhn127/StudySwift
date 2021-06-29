ed2c497a0e2e8174ac32202bea9a29be23e618b3
kwanghun <kh.ahn@quintet.co.kr>
2021-06-29 10:16:32 +0900

    add- adding to design in node

--- Project_Study/FirstApp/firstApp/Setup/Player/PlayerNode.swift
+++ Project_Study/FirstApp/firstApp/Setup/Player/PlayerNode.swift
@@ -11,55 +11,162 @@ import UIKit
 
 class PlayerNode : ASDisplayNode {
     
-    lazy var headerNode : ASStackLayoutSpec = {
-        let titleNode = ASTextNode()
-        titleNode.textContainerInset = .init(top: 0, left: 10, bottom: 0, right: 10)
-        titleNode.attributedText = .init(string: "인원수")
-        
-        let inputNode = ASEditableTextNode()
-        inputNode.textContainerInset = .init(top: 0, left: 10, bottom: 0, right: 10)
-        inputNode.backgroundColor = UIColor.white
-        
-       return ASStackLayoutSpec(
-        direction: .horizontal,
-        spacing: 0,
-        justifyContent: .spaceBetween,
-        alignItems: .stretch,
-        children: [
-            titleNode.styled({
-                $0.width = .init(unit: .points, value: 60)
-                $0.height = .init(unit: .fraction, value: 1)
-            }),
-            inputNode.styled({
-                $0.width = .init(unit: .fraction, value: 1)
-                $0.height = .init(unit: .fraction, value:1)
-            }),
-        ]).styled({
-            $0.width = .init(unit: .fraction, value: 1)
-            $0.height = .init(unit: .points, value: 20)
-        })
+    lazy var inputNode : ASEditableTextNode = {
+        let node = ASEditableTextNode()
+        node.textContainerInset = .init(top: 0, left: 0, bottom: 0, right: 0)
+        node.backgroundColor = UIColor.white
+        node.borderWidth = 1
+        node.borderColor = UIColor.lightGray.cgColor
+        return node
     }()
     
+    lazy var headerNode : ASDisplayNode = {
+        let node = ASDisplayNode()
+        let titleNode = ASTextNode()
+        titleNode.textContainerInset = .init(top: 0, left: 0, bottom: 0, right: 0)
+        titleNode.attributedText = .init(string: "인원수")
+        
+       
+        node.automaticallyManagesSubnodes = true
+        node.automaticallyRelayoutOnSafeAreaChanges = true
+        node.automaticallyRelayoutOnLayoutMarginsChanges = true
+        node.layoutSpecBlock = { [weak self] (_,_) in
+            guard let self = self else { return ASLayoutSpec() }
+            return ASStackLayoutSpec(
+                direction: .horizontal,
+                spacing: 0,
+                justifyContent: .spaceBetween,
+                alignItems: .stretch,
+                children: [
+                    titleNode.styled({
+                        $0.width = .init(unit: .points, value: 60)
+                        $0.height = .init(unit: .fraction, value: 1)
+                        }),
+                    self.inputNode.styled({
+                        $0.width = .init(unit: .fraction, value: 0.5)
+                        $0.height = .init(unit: .fraction, value:1)
+                        }),
+                    ]
+                )
+        }
+        return node
+    }()
+    
+    var model :[PlayerModel] = []
+    
+    lazy var collectionNode : DataCollectionNode<PlayerModel,PlayerCellNode> = {
+        let node = DataCollectionNode<PlayerModel,PlayerCellNode>()
+       
+        model.append(.init(name: "test"))
+        model.append(.init(name: "test2"))
+        node.model = model
+        node.backgroundColor = UIColor.clear
+        return node
+    }()
+
+    lazy var footerNode : ASButtonNode = {
+        let node = ASButtonNode()
+        node.contentEdgeInsets = .init(top: 0, left: 30, bottom: 0, right: 30)
+        node.setAttributedTitle(.init(string: "NEXT"), for: .normal)
+        node .addTarget(self, action: #selector(ButtonDidPress), forControlEvents: .touchUpInside)
+        node.borderWidth = 2
+        node.borderColor = UIColor.lightText.cgColor
+        return node
+    }()
+    
+    @objc func ButtonDidPress(_ button: ASButtonNode) {
+        onNext()
+    }
     override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        self.borderColor = UIColor.blue.cgColor
+        self.borderWidth = 1
         return ASInsetLayoutSpec(
-            insets: .zero,
+            insets: .init(top: 40, left: 10, bottom: 40, right: 10),
             child: ASStackLayoutSpec(
                 direction: .vertical,
                 spacing: 0,
                 justifyContent: .center,
-                alignItems: .start,
+                alignItems: .stretch,
                 children: [
-                    headerNode,
+                    headerNode.styled({
+                        $0.width = .init(unit: .fraction, value: 1)
+                        $0.height = .init(unit: .points, value: 20)
+                    }),
+                    ASDisplayNode().styled({
+                        $0.width = .init(unit: .fraction, value: 1)
+                        $0.height = .init(unit: .points, value: 8)
+                    }).setBackgroundColor(color: UIColor.black),
+                    collectionNode.styled({
+                        $0.width = .init(unit: .fraction, value: 1)
+                        $0.height = .init(unit: .fraction, value: 1)
+                    }),
+                    footerNode.styled({
+                        $0.width = .init(unit: .fraction, value: 1)
+                        $0.height = .init(unit: .points, value: 20)
+                    }),
+                    ASDisplayNode().styled({
+                        $0.width = .init(unit: .fraction, value: 1)
+                        $0.height = .init(unit: .points, value: 20)
+                    }),
                 ])
-        )
+            )
     }
     
-    override init() {
+    let onNext: ()->()
+    init(onNext: @escaping ()->() = {} ) {
+        //comment escaping
+        self.onNext = onNext
         super.init()
         self.automaticallyManagesSubnodes = true
         self.automaticallyRelayoutOnSafeAreaChanges = true
         self.automaticallyRelayoutOnLayoutMarginsChanges = true
     }
-    
+
+    override func didLoad() {
+        let toolBarKeyboard = UIToolbar()
+        let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: .none, action: .none)
+        let btnDoneBar = UIBarButtonItem(title: "OK", style: .done, target: self, action: #selector(self.create))
+        toolBarKeyboard.items = [space,btnDoneBar]
+        toolBarKeyboard.tintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        toolBarKeyboard.backgroundColor = .init(hexString: "#8c79b4")
+        //#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+        toolBarKeyboard.sizeToFit()
+
+        inputNode.textView.inputAccessoryView = toolBarKeyboard
+        //comment 최대 라인 수
+        inputNode.textView.textContainer.maximumNumberOfLines = 1
+        // Do any additional setup after loading the view.
+
+    }
+    @objc func create() {
+            let number = (inputNode.attributedText?.string.intValue ?? 0 ) as Int
+            model.removeAll()
+            for index in 0 ..< number
+            {
+                model.append(PlayerModel(name: "사람\(index + 1)"))
+            }
+            collectionNode.model = model
+            collectionNode.collectionNode.reloadData()
+            self.view.endEditing(true)
+        }
 
 }
+extension SetupViewController : ASEditableTextNodeDelegate
+{
+    func editableTextNodeShouldBeginEditing(_ editableTextNode: ASEditableTextNode) -> Bool {
+        return true
+    }
+    func editableTextNode(_ editableTextNode: ASEditableTextNode, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let nsString = editableTextNode.textView.text as NSString?
+        guard
+            let newString = nsString?.replacingCharacters(in: range, with: text),
+            newString.lengthOfBytes(using: .utf8) <= 1
+        else {
+            return false
+        }
+        return true
+    }
+}
+
+   
+
--- Project_Study/FirstApp/firstApp/Setup/Winning/WinningNode.swift
+++ Project_Study/FirstApp/firstApp/Setup/Winning/WinningNode.swift
@@ -10,28 +10,21 @@ import AsyncDisplayKit
 import UIKit
 
 class WinningNode : ASDisplayNode {
-//    lazy var titleNode : ASTextNode = {
-//        let node = ASTextNode()
-//        node.textContainerInset = .init(top: 0, left: 10, bottom: 0, right: 10)
-//        node.attributedText = .init(string: "인원수")
-//        return node
-//    }()
+
     
-//    lazy var inputNode : ASEditableTextNode = {
-//        let node = ASEditableTextNode()
-//        node.textContainerInset = .init(top: 0, left: 10, bottom: 0, right: 10)
-//        node.backgroundColor = UIColor.white
-//        return node
-//    }()
+    lazy var inputNode : ASEditableTextNode = {
+        let node = ASEditableTextNode()
+        node.textContainerInset = .init(top: 0, left: 0, bottom: 0, right: 0)
+        node.backgroundColor = UIColor.white
+        node.borderWidth = 1
+        node.borderColor = UIColor.lightGray.cgColor
+        return node
+    }()
     
     lazy var headerNode : ASStackLayoutSpec = {
         let titleNode = ASTextNode()
         titleNode.textContainerInset = .init(top: 0, left: 10, bottom: 0, right: 10)
         titleNode.attributedText = .init(string: "당첨자 수")
-        
-        let inputNode = ASEditableTextNode()
-        inputNode.textContainerInset = .init(top: 0, left: 10, bottom: 0, right: 10)
-        inputNode.backgroundColor = UIColor.white
         
        return ASStackLayoutSpec(
         direction: .horizontal,
@@ -43,7 +36,7 @@ class WinningNode : ASDisplayNode {
                 $0.width = .init(unit: .points, value: 60)
                 $0.height = .init(unit: .fraction, value: 1)
             }),
-            inputNode.styled({
+            self.inputNode.styled({
                 $0.width = .init(unit: .fraction, value: 1)
                 $0.height = .init(unit: .fraction, value:1)
             }),
@@ -53,21 +46,87 @@ class WinningNode : ASDisplayNode {
         })
     }()
     
+    var model :[WinningModel] = []
+    
+    lazy var collectionNode : DataCollectionNode<WinningModel,WinningCellNode> = {
+        let node = DataCollectionNode<WinningModel,WinningCellNode>()
+       
+        model.append(.init(name: "test"))
+        model.append(.init(name: "test2"))
+        model.append(.init(name: "test3"))
+        model.append(.init(name: "test4"))
+        
+        node.model = model
+        node.backgroundColor = UIColor.clear
+        return node
+    }()
+
+    lazy var footerNode : ASDisplayNode = {
+        let node = ASDisplayNode()
+        let back = ASButtonNode()
+        back.contentEdgeInsets = .init(top: 0, left: 30, bottom: 0, right: 30)
+        back.setAttributedTitle(.init(string: "Back"), for: .normal)
+        back.addTarget(self, action: #selector(goBackButtonDidPress), forControlEvents: .touchUpInside)
+        back.borderWidth = 2
+        back.borderColor = UIColor.red.cgColor
+        
+        let product = ASButtonNode()
+        back.contentEdgeInsets = .init(top: 0, left: 30, bottom: 0, right: 30)
+        back.setAttributedTitle(.init(string: "생성"), for: .normal)
+        back.addTarget(self, action: #selector(produceButtonDidPress), forControlEvents: .touchUpInside)
+        back.borderWidth = 2
+        back.borderColor = UIColor.red.cgColor
+        return node
+    }()
+    
+    @objc func goBackButtonDidPress(_ button: ASButtonNode) {
+        goBack()
+    }
+    
+    @objc func produceButtonDidPress(_ button: ASButtonNode) {
+        toProduct()
+    }
     override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        self.borderColor = UIColor.blue.cgColor
+        self.borderWidth = 1
         return ASInsetLayoutSpec(
-            insets: .zero,
+            insets: .init(top: 40, left: 10, bottom: 40, right: 10),
             child: ASStackLayoutSpec(
                 direction: .vertical,
                 spacing: 0,
                 justifyContent: .center,
-                alignItems: .start,
+                alignItems: .stretch,
                 children: [
-                    headerNode,
+                    headerNode.styled({
+                        $0.width = .init(unit: .fraction, value: 1)
+                        $0.height = .init(unit: .points, value: 20)
+                    }),
+                    ASDisplayNode().styled({
+                        $0.width = .init(unit: .fraction, value: 1)
+                        $0.height = .init(unit: .points, value: 8)
+                    }).setBackgroundColor(color: UIColor.black),
+                    collectionNode.styled({
+                        $0.width = .init(unit: .fraction, value: 1)
+                        $0.height = .init(unit: .fraction, value: 1)
+                    }),
+                    footerNode.styled({
+                        $0.width = .init(unit: .fraction, value: 1)
+                        $0.height = .init(unit: .points, value: 20)
+                    }),
+                    ASDisplayNode().styled({
+                        $0.width = .init(unit: .fraction, value: 1)
+                        $0.height = .init(unit: .points, value: 20)
+                    }),
                 ])
-        )
+            )
     }
     
-    override init() {
+    let toProduct: ()->()
+    let goBack: ()->()
+    
+    init(toProduct: @escaping ()->() = {} , goBack: @escaping ()->() = {}) {
+        self.toProduct = toProduct
+        self.goBack = goBack
         super.init()
         self.automaticallyManagesSubnodes = true
         self.automaticallyRelayoutOnSafeAreaChanges = true
